bc0300a267543880cdcc9e06f0e66718a0495809
e1305025096 <e1305025096@oit.ac.jp>
2026-02-26 18:45:52 +0900

    ルーム名前表示処理修正

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82144493,
+    wasmFileSize: 82144569,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
