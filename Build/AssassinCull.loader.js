8847f9c6869ef2cba8963763b651bf2c3c94bde0
e1305025096 <e1305025096@oit.ac.jp>
2026-02-27 12:51:55 +0900

    2回目以降ルーム入退出処理修正 var0.1.3

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82146254,
+    wasmFileSize: 82146170,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
