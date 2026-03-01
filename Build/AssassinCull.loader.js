ec57f61a41bc337be816536e7251624bc547f765
e1305025096 <e1305025096@oit.ac.jp>
2026-03-01 14:05:43 +0900

    var0.2.2 カード配るシステム修正

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82283969,
+    wasmFileSize: 82284089,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
