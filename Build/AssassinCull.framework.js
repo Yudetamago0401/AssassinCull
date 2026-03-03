29200c04d57135889169a40220adac1f824ebcbe
e1305025096 <e1305025096@oit.ac.jp>
2026-03-03 17:05:24 +0900

    var0.2.15 カードの使用を開始する

--- Build/AssassinCull.framework.js
+++ Build/AssassinCull.framework.js
@@ -1333,10 +1333,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  6327328: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 6327383: ($0) => { performance.now = function() { return $0; }; },  
- 6327431: ($0) => { performance.now = function() { return $0; }; },  
- 6327479: () => { performance.now = Module['emscripten_get_now_backup']; }
+  6328448: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 6328503: ($0) => { performance.now = function() { return $0; }; },  
+ 6328551: ($0) => { performance.now = function() { return $0; }; },  
+ 6328599: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82445717,
+    wasmFileSize: 82469861,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
