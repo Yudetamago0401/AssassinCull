3040dbe2a37235e05a923d2b786d1a71148f671b
e1305025096 <e1305025096@oit.ac.jp>
2026-03-05 12:56:34 +0900

    var0.3.18 リザルトのリトライ機能修正

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82447456,
+    wasmFileSize: 82447604,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
