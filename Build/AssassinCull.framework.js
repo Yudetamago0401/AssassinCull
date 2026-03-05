707d86afeccd9a5c163356f6b7cd88fbf57c8a78
e1305025096 <e1305025096@oit.ac.jp>
2026-03-05 15:29:15 +0900

    var0.3.22 リザルトのリトライ機能修正

--- Build/AssassinCull.framework.js
+++ Build/AssassinCull.framework.js
@@ -1333,10 +1333,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  6327200: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 6327255: ($0) => { performance.now = function() { return $0; }; },  
- 6327303: ($0) => { performance.now = function() { return $0; }; },  
- 6327351: () => { performance.now = Module['emscripten_get_now_backup']; }
+  6327088: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 6327143: ($0) => { performance.now = function() { return $0; }; },  
+ 6327191: ($0) => { performance.now = function() { return $0; }; },  
+ 6327239: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82447265,
+    wasmFileSize: 82445604,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
