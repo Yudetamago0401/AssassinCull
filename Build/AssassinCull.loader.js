7e3bf47eb1f8981c512d49d298ee88fdabfa3445
e1305025096 <e1305025096@oit.ac.jp>
2026-03-06 14:04:19 +0900

    var0.4.6 殺戮カードは5日以前に使えないようにした

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82450169,
+    wasmFileSize: 82450486,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
