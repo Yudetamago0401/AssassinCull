eaff2fe0403040e9b9fa0559aa44edafaed3f42c
e1305025096 <e1305025096@oit.ac.jp>
2026-03-06 09:52:30 +0900

    var0.4.4 リザルトシーンでのマスタークライアントの切り替わりとその通知

--- Build/AssassinCull.framework.js
+++ Build/AssassinCull.framework.js
@@ -1333,10 +1333,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  6327104: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 6327159: ($0) => { performance.now = function() { return $0; }; },  
- 6327207: ($0) => { performance.now = function() { return $0; }; },  
- 6327255: () => { performance.now = Module['emscripten_get_now_backup']; }
+  6327312: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 6327367: ($0) => { performance.now = function() { return $0; }; },  
+ 6327415: ($0) => { performance.now = function() { return $0; }; },  
+ 6327463: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82448084,
+    wasmFileSize: 82450149,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
