519fdefea0a289323fc63760b4ef8528ceae2e38
e1305025096 <e1305025096@oit.ac.jp>
2026-03-07 00:53:56 +0900

    var0.4.27 拾得カードの仕様修正

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82503265,
+    wasmFileSize: 82503239,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
