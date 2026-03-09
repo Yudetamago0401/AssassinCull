d7361a892163622240a05817eec821538f7cbcee
e1305025096 <e1305025096@oit.ac.jp>
2026-03-09 17:28:07 +0900

    var0.4.38 探偵カードの仕様修正

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82536090,
+    wasmFileSize: 82536699,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
