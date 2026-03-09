f3470276260c25f6605b5762cb004d8fa2379ddf
e1305025096 <e1305025096@oit.ac.jp>
2026-03-09 13:35:25 +0900

    var0.4.36 平和カードの仕様修正

--- Build/AssassinCull.loader.js
+++ Build/AssassinCull.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82523481,
+    wasmFileSize: 82523516,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
